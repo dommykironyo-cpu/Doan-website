ec38250596048b5b460687f7f65f66cad8f7c504
dommykironyo-cpu <dommykironyo@gmail.com>
2025-11-04 10:13:41 +0300

    Add files via upload

---
+++ doan.js
@@ -0,0 +1,6 @@
+function showMessage() {
+  const messageElement = document.getElementById("message");
+  messageElement.textContent = "Hello Doan! 🎉 You just activated JavaScript!";
+  messageElement.style.color = "green";
+}
+
